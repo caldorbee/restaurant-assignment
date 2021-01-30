893fc6c552e6abb889da60cc3b97024af2931b4a
mburg0s <burgos.marissa@gmail.com>
2021-01-30 16:18:55 -0500

    update the date and aler

--- reservation.js
+++ reservation.js
@@ -1,6 +1,8 @@
-const reserve = document.querySelector("#reserve");
+const reserve = document.querySelector("#reserve")
+const fullname = document.querySelector("#fullname")
 
 reserve.addEventListener("click", function (e) {
-  //   e.preventDefault();
-  alert("Your reservation has been submitted!");
-});
+   
+  e.preventDefault()
+  alert('Your reservation has been submitted!')
+})
