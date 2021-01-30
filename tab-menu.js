ecb1d94f4e1f965594af4aa2b7795045f7b38e7d
caldorbee <calvinbeene@gmail.com>
2021-01-30 10:12:23 -0500

    stash

--- tab-menu.js
+++ tab-menu.js
@@ -14,75 +14,16 @@ btnTab.addEventListener('click',function(e) {
         divToShow.hidden = false
 })
 
-const btnMenu = document.querySelector('#menu-select')
-btnMenu.addEventListener('click',function(e){
-    const buttonTarget = e.target
-    console.log(buttonTarget)
-    const menuTarget = buttonTarget.dataset.target
-    const menuToShow = document.querySelector('#'+menuTarget)
-    const menuElements = document.querySelectorAll('.menu-Con')
-console.log(menuElements)
-    menuElements.forEach(function(elm){
-        elm.hidden = true
-    })
-        menuToShow.hidden = false
-})
-
 
 const menu = document.querySelector('#buttonCon-men')
-const appetizer = document.querySelector('#menuCon-appetizer')
-const lunch = document.querySelector('#menuCon-lunch')
-const dinner = document.querySelector('#menuCon-dinner')
-const dessert = document.querySelector('#menuCon-dessert')
-
-
-// const dessert = document.querySelector('#desserts')
-
-fetch('https://obscure-tundra-54269.herokuapp.com/casual-dining')
-.then((response)=>response.json())
-.then(function(data){
-    const dnr = data.entrees.map(function(x){
-
-        return ` 
-        <div class= "name-price">
-            <span class ="name">${x.name}</span> <span class ="dot">..................................................................... </span> <span class ="price"> $${x.price}</span>
-        </div
-        <span class ="desc">${x.description}</span>  `
-    })
-    dinner.innerHTML=dnr.join('')
-})
-
 fetch('https://obscure-tundra-54269.herokuapp.com/bar-food')
 .then((resp)=> resp.json())
 .then(function (data) {
-    const appetizers = data.appetizers.map(function(item){
-        return `
-                    <div class= "name-price">
-                        <span class ="name">${item.name}</span> <span class ="dot">..................................................................... </span> <span class ="price"> $${item.price}</span>
-                    </div>  
-                        <span class ="desc">${item.description}</span>
-                    
-                     `
+    console.log(data.entrees)
+    const entrees = data.entrees.map(function(item){
+        return `<p> -- ENTREES --- </p>
+                <p>${item.name} ....................... ${item.price}</p>
+                    <p>${item.description}</p>  `
     })
-    const lunchs = data.entrees.map(function(item){ 
-        // console.log(item.name)
-        return `
-                 <div class= "name-price">
-                    <span class ="name">${item.name} </span> <span class ="dot">..................................................................... </span> <span class ="price"> $${item.price}</span>
-                </div>
-                <p class="desc">${item.description}</p>  `
-    })
-    const desserts= data.desserts.map(function(item){
-        return `
-
-        <div class= "name-price">
-                <span class ="name">${item.name} </span> <span class ="dot">..................................................................... </span> <span class ="price"> $${item.price}</span>
-        </div>    
-                <p class = "desc">${item.description}</p>  `
-    })
-
-    
-    appetizer.innerHTML=appetizers.join('') 
-    lunch.innerHTML= lunchs.join('')
-    dessert.innerHTML=desserts.join('')
+    menu.innerHTML=entrees.join('')
 })
