175eab8873fb56cfc571a64d30da6c13b10c59d9
Candice James-Waples <76015275+CTJW@users.noreply.github.com>
2021-01-31 12:45:13 -0500

    no changes

--- reservation.js
+++ reservation.js
@@ -1,10 +1,10 @@
-const reserve = document.querySelector("#reserve")
-const fullname = document.querySelector("#fullname")
-const form = document.querySelector("#form")
+const reserve = document.querySelector("#reserve");
+const fullname = document.querySelector("#fullname");
+const form = document.querySelector("#form");
 
 form.addEventListener("submit", function (e) {
-  e.preventDefault()
-  if (fullname.textContent=='') {
-    alert("Name is required")
-  }else alert('Your reservation has been submitted!')
-})
+  e.preventDefault();
+  if (fullname.textContent == "") {
+    alert("Name is required");
+  } else alert("Your reservation has been submitted!");
+});
