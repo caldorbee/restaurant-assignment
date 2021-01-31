d45fc76ec90aba3bae565a8a02be998382c615ef
punchcode <punchcode@punchcodes-MacBook-Pro.local>
2021-01-31 17:19:25 -0500

    reservation date required and reset form

--- reservation.js
+++ reservation.js
@@ -1,10 +1,16 @@
 const reserve = document.querySelector("#reserve");
 const fullname = document.querySelector("#fullname");
+const date = document.querySelector("#date");
 const form = document.querySelector("#form");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  if (fullname.textContent == "") {
-    alert("Name is required");
-  } else alert("Your reservation has been submitted!");
+  if (fullname.value == "") {
+    alert("Name is required!");
+  } else if (date.value == ""){
+    alert("Date is required!");
+  }else
+  alert("Your reservation has been submitted!");
+  document.forms['form'].reset()
 });
+
